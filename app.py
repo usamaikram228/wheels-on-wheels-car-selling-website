22c9f035cd5dd68750571132ee3e1017575dab00
Abdullah <122647095+Abdullah5111@users.noreply.github.com>
2023-01-15 18:25:07 +0500

    Testing clone

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
 	return render_template('homepage.html')
 
+@app.route('/test')
+def test():
+	return render_template('test.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
